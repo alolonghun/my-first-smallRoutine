db658b575b1fc3bb3bac99d82af1cb2f814da9a7
alolonghun <906268297@qq.com>
2017-07-30 15:48:43 +0800

    我的第一个小程序

---
+++ data/posts-data.js
@@ -0,0 +1,120 @@
+var local_database = [
+    {
+        date: "Mar 18 2017",
+        title: "高楼大劫案，什么鬼",
+        imgSrc: "/images/post/gldja.jpg",
+        avatar: "/images/avatar/1.jpg",
+        content: "太荒诞了，从角色设定、犯罪动机、故事情节到动作戏桥段处处透着一股荒诞劲儿，你只能暗自佩服两位编剧的想象力，有了一个现代罗宾汉的想法就能轻而易举的替天行道，楼下满大街的游行队伍就没有会看得见阁楼顶上悬挂的法拉利？不是犯罪看起来这么简单，是因为电影主创们太傻太天真。",
+        reading: "112",
+        collection: "96",
+        headImgSrc: "/images/post/gldja.jpg",
+        author: "sky",
+        dateTime: "1个月前",
+        detail: "太荒诞了，从角色设定、犯罪动机、故事情节到动作戏桥段处处透着一股荒诞劲儿，你只能暗自佩服两位编剧的想象力，有了一个现代罗宾汉的想法就能轻而易举的替天行道，楼下满大街的游行队伍就没有会看得见阁楼顶上悬挂的法拉利？不是犯罪看起来这么简单，是因为电影主创们太傻太天真。最好玩的一个细节：破产银行家是由Matthew Broderick扮演的，看的时候我一直觉得这哥们眼熟。而偷运红色保时捷时，我想到了《春天不是读书天》里的红色保时捷。看完电影查演员表时恍然大悟，本片与《春天不是读书天》的重合不仅仅是一部红色保时捷，《春天》的主角就是Matthew Broderick!!",
+        postId: 0,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+            title: "夜夜夜夜-齐秦",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "加勒比海盗，这一次干得漂亮",
+        content: "‘因为地平线一直在那。你想到达那儿，但你永远到达不了。就是那样，遥不可及难以放弃。’Johnny说看过的一部法国水手写的书中，问及到为什么选择当水手一直漂泊时，那个水手如此回答。",
+        imgSrc: "/images/post/jlb.jpg",
+        reading: 62,
+        detail: "‘因为地平线一直在那。你想到达那儿，但你永远到达不了。就是那样，遥不可及难以放弃。’Johnny说看过的一部法国水手写的书中，问及到为什么选择当水手一直漂泊时，那个水手如此回答。所以，杰克船长最后的一句话是：现在朝那地平线出发！我爱上了这段话，也突然明白了一直选择边缘角色的Johnny，为什么会接这部电影。我被这段话感动了。他说出了我心里的一些东西。我在这么久之后，因为这段话被这部电影感动了。地平线是自由。",
+        collection: 92,
+        dateTime: "1个月前",
+        headImgSrc: "/images/post/jlb.jpg",
+        author: "迷的城",
+        date: "Mar 20 2017",
+        avatar: "/images/avatar/2.jpg",
+        postId: 1,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+            title: "鬼迷心窍-李宗盛",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "一场游戏电影",
+        content: "9号二刷回来补充，还是那个感觉，挺好的，作为电影而言，莱恩国王演的不是很舒服，加罗娜和洛萨的爱情戏太强行，如果不知道麦迪文身世的非玩家看不懂麦迪文的戏份，应该大概就这些吧。",
+        detail: "9号二刷回来补充，还是那个感觉，挺好的，作为电影而言，莱恩国王演的不是很舒服，加罗娜和洛萨的爱情戏太强行，如果不知道麦迪文身世的非玩家看不懂麦迪文的戏份，应该大概就这些吧。顺便一说，我就知道一个个的又跟我扯历史了，好好好，就你们知道的多，你们是暴雪员工，你们跟着编的魔兽正史好了吧？我这不是影评，我不敢把自己的吐槽放到这么高的位置，只是一个普通魔兽玩家对那些所谓正视历史的黑粉们的不满从而发出的简单的吐槽，大家都是零点看首映的人，谁没个情怀，但是您拿出情怀来嘲笑我就有点不妥了，各位都成年了，别用小孩子吵架的方式来，谢谢咯！最后，继续对黑电影的所谓粉丝们说，看图！",
+        imgSrc: "/images/post/ms.jpg",
+        headImgSrc: "/images/post/ms.jpg",
+        reading: 62,
+        collection: 92,
+        author: "知乎用户",
+        date: "Mar 22 2017",
+        dateTime: "28天前",
+        avatar: "/images/avatar/3.jpg",
+        postId: 2,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+            title: "女儿情-万晓利",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "神一样的范迪塞尔",
+        content: "不得不佩服电影艺术的神奇，往往有一种化腐朽为神奇的功效，从一粒种子到参天大树，可以见证奇迹的发生。",
+        imgSrc: "/images/banner/1.jpg",
+        reading: 102,
+        detail: "不得不佩服电影艺术的神奇，往往有一种化腐朽为神奇的功效，从一粒种子到参天大树，可以见证奇迹的发生。“速激”系列就是这样的一个奇迹，它从当初 第一部的起，本是一个非常不起眼的苗子，还曾经长歪过，结果还是在扭正之后长成了枝繁叶茂的一支奇葩，并且成为环球影业的最亮的招牌系列。系列电影在好莱坞是一个具有独特魅力的制作，而且往往是票房的保证，甚至挽制片公司于狂澜，像克雷格版的007就拯救了濒死的米高梅。在传统的美国6大电影 公司里，每家公司都拥有自己的招牌系列，而身为6大之一的环球，在“速激”之前，近些年间并没有扶植起像样的招牌系列。所以，当“速激”好不容易成为吸金 招牌之后，环球对于这个系列就视为珍宝。但回看一下《速度与激情》出品的2001年，那年的环球，更大投资的电影就有《侏罗纪公园3》、《木乃伊2》、 《汉尼拔》、《间谍游戏》、《拜见岳父大人》、《美丽心灵》等这样的叫好叫座的片子，厕身于其间的《速度与激情》，在当时没有大明星，没有大导演，没有名 编剧，也没有被投入许多钱，几乎什么可以拿得出手的东西，完全是无心插柳之作，尤其是纯粹为套钱的第2、3部的出炉，更是坐实了这种看法。",
+        collection: 92,
+        dateTime: "10天前",
+        headImgSrc: "/images/banner/1.jpg",
+        author: "阿猫阿狗",
+        date: "Apr 10 2017",
+        avatar: "/images/avatar/4.jpg",
+        postId: 3,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+            title: "恋恋风尘-老狼",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+        }
+    },
+    {
+        title: "现实不能承受之轻",
+        content: "在《蝙蝠侠初养成》（Batman Begins）之前，恐怕没人有过这方向的全面尝试。毕竟，漫画英雄这种注定要在不断的狗血情节中涅槃轮回的东东，实在离现实有些远。",
+        imgSrc: "/images/banner/2.jpg",
+        reading: 102,
+        detail: "在《蝙蝠侠初养成》（Batman Begins）之前，恐怕没人有过这方向的全面尝试。毕竟，漫画英雄这种注定要在不断的狗血情节中涅槃轮回的东东，实在离现实有些远。但是，诺兰哥非常聪明地发现了漫画与现实最易互通之处，那就是角色的心路历程以及由此传递的可以让观众感同身受的角色情感。至于基调的黑暗，那个其实和“现实感”没有必然的逻辑关联。当然，在后9.11时代，任何对“英雄”的演绎，都会被多愁善感的人士赋予现实意义——这在美国“文艺评论”这个最多愁善感的圈子里更无法避免。所以，到2008年《暗黑骑士》（The Dark Knight）上映时，舆论似乎更加认定诺兰是想在完全现实的设定里“重新定义”超级英雄。小丑那无法捉摸的极端无政府主义疯狂，不就是现实中让美国人民心烦心悸、却又无法根除的恐怖主义隐患么？",
+        collection: 26,
+        dateTime: "8天前",
+        headImgSrc: "/images/banner/2.jpg",
+        author: "小点点",
+        date: "Apr 12 2017",
+        avatar: "../../../images/avatar/5.jpg",
+        postId: 4,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "沉默是金-张国荣",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "雷神2——简单粗暴",
+        content: "漫威的电影真的是越来越成熟了，《雷神2》就像是工厂流水线上打造出的作品，简直像是拿尺子一分一毫度量拍出来的。哪里该有笑点，哪里该打架，哪里该文戏，哪里该卖腐，各部分安排的极其标准，不会出错，但是也太predictable了……",
+        imgSrc: "/images/banner/3.jpg",
+        reading: 96,
+        detail: "漫威的电影真的是越来越成熟了，《雷神2》就像是工厂流水线上打造出的作品，简直像是拿尺子一分一毫度量拍出来的。哪里该有笑点，哪里该打架，哪里该文戏，哪里该卖腐，各部分安排的极其标准，不会出错，但是也太predictable了……看得出《雷神2》非常讨巧粉丝，给loki和thor之间的化学反应加成了不止几倍啊。漫画没看过不清楚，但我相信应该也是很让漫画迷们满意的吧？在满足粉丝同时对于普通观众也做到了考虑，动作戏不少，节奏不拖沓，入之前所说的非常标准不出错，爱看大片的应该不会失望。而我即便觉得本片一般，你让我挑刺，我也挑不出什么，《雷神2》的确是部爆米花佳作，但就是没能让我留下什么特别深的印象。",
+        collection: 26,
+        dateTime: "21小时前",
+        headImgSrc: "/images/banner/3.jpg",
+        author: "深蓝色",
+        date: "Apr 20 2017",
+        avatar: "../../../images/avatar/6.jpg",
+        postId: 5,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+            title: "朋友-谭咏麟",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
+    },
+]
+
+module.exports = {
+    postList: local_database
+}
---
+++ pages/movies/movies.js
@@ -0,0 +1,29 @@
+var app = getApp();
+
+Page({
+  onLoad: function (event) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/ in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
+
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+  },
+
+  getMovieListData: function (url) {
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "content-type": "application/json"
+      },
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (error) {
+        console.log('fail');
+      }
+    })
+  }
+})
